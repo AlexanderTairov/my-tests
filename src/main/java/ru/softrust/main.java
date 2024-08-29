a22c918e94ada0db9f4d02930df89e40d393c35c
Таиров Александр <atairov@softrust.ru>
2024-08-29 12:19:06 +0300

    added first tests

---
+++ src/main/java/ru/softrust/main.java
@@ -0,0 +1,7 @@
+package ru.softrust;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("I love Ira");
+    }
+}
---
+++ src/test/java/ru/softrust/DemoQATest.java
@@ -0,0 +1,36 @@
+package ru.softrust;
+
+import org.junit.jupiter.api.*;
+
+public class DemoQATest {
+
+    @BeforeAll
+    static void beforeAll() {
+        System.out.println("beforeAlltests");
+    }
+
+    @AfterAll
+    static void afterAll() {
+        System.out.println("afterAllTests");
+    }
+
+    @BeforeEach
+     void beforeEach() {
+        System.out.println("Before test!\n");
+    }
+
+    @AfterEach
+    void afterEach() {
+        System.out.println("after test!\n");
+    }
+
+    @Test
+    void firstTest() {
+        System.out.println("I love Ira");
+    }
+
+    @Test
+    void secondTest() {
+        System.out.println("I love dota");
+    }
+}
