0c907ed489cfa43bf8d8429b93be3384f29c4786
Таиров Александр <atairov@softrust.ru>
2024-08-29 12:21:42 +0300

    added first tests

--- src/main/java/ru/softrust/main.java
+++ src/main/java/ru/softrust/main.java
@@ -1,7 +1,7 @@
 package ru.softrust;
 
 public class main {
-    public static void Main(String[] args) {
+    public static void main(String[] args) {
         System.out.println("I love Ira");
     }
 }
